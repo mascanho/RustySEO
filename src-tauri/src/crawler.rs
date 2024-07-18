657b86ef4d93921388cfb4bbbee586b6b97764c8
mascanho <mascanho@sapo.pt>
2024-07-18 20:34:31 +0100

    updates

--- src-tauri/src/crawler.rs
+++ src-tauri/src/crawler.rs
@@ -407,7 +407,6 @@ pub async fn crawl(mut url: String) -> Result<CrawlResult, String> {
             // println!("{}: {:?}", key, value);
         }
         // Fetch the word count
-
         let word_count_selector =
             Selector::parse("body, h1, h2, h3, h4, h5, h6, p, span, li, div, a").unwrap();
         let mut word_count = 0;
@@ -427,7 +426,7 @@ pub async fn crawl(mut url: String) -> Result<CrawlResult, String> {
     let words_adjusted = (words_amount as f64 * 2.9).round() as usize;
 
     // calculate reading time
-    let reading_time = calculate_reading_time(words_adjusted, 150);
+    let reading_time = content::calculate_reading_time(words_adjusted, 150);
 
     // println!("Links: {:?}", links);
     // println!("Headings: {:?}", headings);
@@ -485,10 +484,6 @@ pub async fn crawl(mut url: String) -> Result<CrawlResult, String> {
         head_elements,
         body_elements,
     })
-}
-
-pub fn calculate_reading_time(word_count: usize, words_per_minute: usize) -> usize {
-    (word_count as f64 / words_per_minute as f64).ceil() as usize
 }
 
 pub async fn get_page_speed_insights(url: String) -> Result<PageSpeedResponse, String> {
