3fdf08fbff48d7bd25933d66aea7e2b611a97a61
mascanho <mascanho@sapo.pt>
2024-07-19 23:31:13 +0100

    Tables

--- app/components/ui/ImageAnalysis.tsx
+++ app/components/ui/ImageAnalysis.tsx
@@ -2,70 +2,84 @@ import openBrowserWindow from "@/app/Hooks/OpenBrowserWindow";
 import React from "react";
 
 const ImageAnalysis = ({ images }: { images: any[] }) => {
+  const imagesWithAltText = images.filter((image) => image.alt_text);
+  const imagesWithoutAltText = images.filter((image) => !image.alt_text);
+
   return (
-    <div className="relative shadow rounded-md overflow-hidden">
-      <h2 className="bg-apple-spaceGray font-semibold text-white rounded-t-md w-full pt-1 text-center overflow-clip">
-        Image Analysis
+    <section className="table_container">
+      <h2 className="text-base text-left pl-1 pt-3 font-bold w-full text-black/60">
+        Image analysis
       </h2>
 
-      <section
-        className={`flex flex-col h-[30em] ${
-          images.length !== 0 ? "bg-white" : "bg-white/40"
-        } overflow-hidden`}
-      >
-        <div className="flex-1 overflow-y-auto">
-          <table className="w-full text-sm">
-            <thead className="sticky top-0 bg-white bg-opacity-100 ">
-              <tr className="border-b">
-                <th className="text-xs w-1/5 border-r px-2 align-middle">
-                  Image
-                </th>
-                <th className="text-xs w-2/5 px-2 border-r align-middle">
-                  Alt Text
-                </th>
-                <th className="text-xs w-1/5 px-2 border-r align-middle">
-                  Size
-                </th>
-                <th className="text-xs w-2/5 px-2 align-middle">Link</th>
-              </tr>
-            </thead>
-            <tbody>
-              {images.map((image: any, index: number) => (
-                <tr className="border-b" key={index}>
-                  <td className="px-2 py-1 text-center">
-                    <a href={image?.link} className="block w-full h-full">
-                      <img
-                        src={image?.link}
-                        alt={image?.alt_text}
-                        className="m-auto w-16 h-12 object-contain"
-                      />
-                    </a>
-                  </td>
-                  <td className="px-2 py-1 text-xs">{image.alt_text}</td>
-                  <td className="px-2 py-1 text-xs text-center">
-                    {image.size_mb} KB
-                  </td>
-                  <td
-                    onClick={() => openBrowserWindow(image.link)}
-                    className="px-2 py-1 cursor-pointer text-sm text-blue-500 underline"
-                  >
-                    {image.link}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div
-          className={`sticky bottom-0 text-xs text-center p-2 border-t rounded-b-md ${
-            images.length === 0 ? "bg-white/40" : "bg-white"
-          }`}
+      <div className="h-full overflow-hidden sticky top-0">
+        <section
+          className={`mx-auto flex flex-col shadow w-full ${images.length === 0 ? "bg-white/40" : "bg-white"}`}
         >
-          <span>Images Found:</span>{" "}
-          <span className="text-apple-blue">{images.length}</span>
+          <div className="relative">
+            <div className="overflow-auto h-[32.8rem]">
+              <table className="w-full">
+                <thead className="text-xs text-left py-2 border-b-2">
+                  <tr>
+                    <th>Image</th>
+                    <th>Alt Text</th>
+                    <th>Size</th>
+                    <th>Link</th>{" "}
+                  </tr>
+                </thead>
+
+                <tbody className="border-t">
+                  {images.map((image: any, index: number) => (
+                    <tr key={index} className="align-middle">
+                      <td className="border-r border-b px-2 py-1 text-center">
+                        <a href={image?.link} className="block w-full h-full">
+                          <img
+                            src={image?.link}
+                            alt={image?.alt_text}
+                            className="m-auto w-16 h-12 object-contain"
+                          />
+                        </a>
+                      </td>
+                      <td className="border-r border-b px-2 py-1 text-xs">
+                        {image.alt_text}
+                      </td>
+                      <td className="border-r border-b px-2 py-1 text-xs text-center">
+                        {image.size_mb} KB
+                      </td>
+                      <td
+                        onClick={() => openBrowserWindow(image.link)}
+                        className="border-b px-2 py-1 cursor-pointer text-sm text-blue-500 underline"
+                      >
+                        {image.link}
+                      </td>
+                    </tr>
+                  ))}{" "}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </section>
+        <div className="pb-1 m-2 rounded-md text-xs flex justify-end text-black/50 space-x-4">
+          <p className="text-xs">
+            Images Found:{" "}
+            <span className="px-1 py-0.5 bg-gray-400 text-white rounded-md">
+              {images.length}
+            </span>
+          </p>
+          <p>
+            Links with Text:{" "}
+            <span className="px-1 py-0.5 bg-green-400 text-white rounded-md">
+              {imagesWithAltText.length}
+            </span>
+          </p>
+          <p>
+            Links without Text:{" "}
+            <span className="px-1 py-0.5 bg-red-400 text-white rounded-md min-w-3">
+              {imagesWithoutAltText.length}
+            </span>
+          </p>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
--- app/components/ui/LinkAnalysis.tsx
+++ app/components/ui/LinkAnalysis.tsx
@@ -2,37 +2,38 @@ import React from "react";
 
 const LinkAnalysis = ({ visibleLinks }: { visibleLinks: any[] }) => {
   return (
-    <section>
-      <h2 className="bg-apple-spaceGray font-semibold text-white rounded-t-md w-full text-center pt-1">
-        Links
+    <section className="table_container">
+      <h2 className="text-base text-left pl-1 pt-3 font-bold w-full text-black/60">
+        Link Analysis
       </h2>
-      <div className="shadow rounded-b-md h-[30rem] overflow-hidden sticky top-0">
+
+      <div className="h-full overflow-hidden sticky top-0">
         <section
           className={`mx-auto flex flex-col shadow w-full ${visibleLinks.length === 0 ? "bg-white/40" : "bg-white"}`}
         >
           <div className="relative">
             <table className="w-full">
-              <thead className="bg-gray-200 text-center text-xs">
+              <thead className="text-xs text-left py-2">
                 <tr>
                   <th className="w-1/3">Anchor</th>
                   <th className="w-2/3">Link</th>
                 </tr>
               </thead>
             </table>
-            <div className="overflow-auto h-[26.5rem]  max-h-[26.5rem]">
+            <div className="overflow-auto h-[30.8rem]">
               <table className="w-full">
                 <tbody>
                   {visibleLinks.map((link, index) => (
                     <tr key={index} className="align-middle">
                       <td className="border-r border-b">
-                        <span className="block py-2 px-3 text-sm w-[180px]">
+                        <span className="block py-1 px-3 text-sm w-[180px]">
                           {link[1] || "-"}
                         </span>
                       </td>
                       <td className="border-b">
                         <a
                           href={link[0]}
-                          className="block py-2 px-3 text-sm text-blue-500"
+                          className="block py-1 px-3 text-sm text-blue-500"
                           aria-label={`Link to ${link[0]}`}
                         >
                           {link[0]}
@@ -44,15 +45,27 @@ const LinkAnalysis = ({ visibleLinks }: { visibleLinks: any[] }) => {
               </table>
             </div>
           </div>
-          <div className="flex items-center justify-center border-t bg-white">
-            <span
-              className={`flex justify-center items-center text-xs w-full p-2 ${visibleLinks.length === 0 ? "bg-white/40" : "bg-white"}`}
-            >
-              Links Found:{" "}
-              <span className="text-blue-500 ml-1">{visibleLinks.length}</span>
-            </span>
-          </div>
         </section>
+        <div className="pb-1 m-2 rounded-md text-xs flex justify-end text-black/50 space-x-4">
+          <p className="text-xs">
+            Links Found:{" "}
+            <span className="px-1 py-0.5 bg-gray-400 text-white rounded-md">
+              {visibleLinks.length}
+            </span>
+          </p>
+          <p>
+            Links with Text:{" "}
+            <span className="px-1 py-0.5 bg-green-400 text-white rounded-md">
+              {visibleLinks.filter((link) => link[1]).length}
+            </span>
+          </p>
+          <p>
+            Links without Text:{" "}
+            <span className="px-1 py-0.5 bg-red-400 text-white rounded-md min-w-3">
+              {visibleLinks.filter((link) => !link[1]).length}
+            </span>
+          </p>
+        </div>
       </div>
     </section>
   );
