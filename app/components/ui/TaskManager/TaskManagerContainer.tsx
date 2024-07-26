c6fe92f9659b13980b2c37a414f0e65ebe54b1d3
mascanho <mascanho@sapo.pt>
2024-07-27 00:03:57 +0100

    UX / UI

--- app/components/ui/TaskManager/TaskManagerContainer.tsx
+++ app/components/ui/TaskManager/TaskManagerContainer.tsx
@@ -16,7 +16,27 @@ type Task = {
 
 const TaskManagerContainer = () => {
   return (
-    <div className="grid grid-cols-3 p-5 w-full gap-8 h-screen bg-slate-100 dark:bg-brand-darker rounded-xl max-w-[1800px] mx-auto">
+    <div className="grid grid-cols-3 p-7 w-full gap-8 h-screen bg-slate-100 dark:bg-brand-darker rounded-xl max-w-[1800px] mx-auto">
+      <section className="w-full border bg-white dark:bg-brand-normal rounded-xl">
+        <div className="flex items-center border-b justify-between px-6 py-5 font-semibold">
+          <h1 className="text-xl">TODO</h1>
+          <FaCirclePlus className="text-2xl text-brand-highlight dark:text-brand-dark" />
+        </div>
+        <div className="p-3">
+          {/* @ts-ignore */}
+          <TodoContainerItems status={"todo"} />
+        </div>
+      </section>
+      <section className="w-full border bg-white dark:bg-brand-normal rounded-xl">
+        <div className="flex items-center border-b justify-between px-6 py-5 font-semibold">
+          <h1 className="text-xl">TODO</h1>
+          <FaCirclePlus className="text-2xl text-brand-highlight dark:text-brand-dark" />
+        </div>
+        <div className="p-3">
+          {/* @ts-ignore */}
+          <TodoContainerItems status={"todo"} />
+        </div>
+      </section>
       <section className="w-full border bg-white dark:bg-brand-normal rounded-xl">
         <div className="flex items-center border-b justify-between px-6 py-5 font-semibold">
           <h1 className="text-xl">TODO</h1>
