f51287bb46194160f8bd7822c94a35e081062371
mascanho <mascanho@sapo.pt>
2024-10-05 16:14:50 +0100

    Revert "Linux user agent added"
    This reverts commit dc8485408f7cefd2a47dc3987f3df2d34730e5ce.

--- src-tauri/src/crawler/content.rs
+++ src-tauri/src/crawler/content.rs
@@ -266,7 +266,7 @@ impl SERP {
         headers.insert(
             "User-Agent",
             HeaderValue::from_static(
-                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.97 Safari/537.36"
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.97 Safari/537.36"
             ),
         );
 
