1d712f85886b1aaa51af5d186d57db2542f75b2b
mascanho <mascanho@sapo.pt>
2025-02-26 17:05:50 +0000

    Update HistoryDomainCrawls.tsx

--- app/global/_components/Sidebar/BottomContainer/HistoryDomainCrawls.tsx
+++ app/global/_components/Sidebar/BottomContainer/HistoryDomainCrawls.tsx
@@ -133,13 +133,6 @@ const HistoryDomainCrawls = () => {
         </div>
       )}
       <div className="text-center">
-        <button
-          onClick={addDataToDatabase}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Add to DB
-        </button>
-
         <div>
           {crawlHistory.length === 0 && !error && (
             <p className="text-gray-500">No crawl history available.</p>
