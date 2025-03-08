662793349910e475692c8a6cad860c5330fa3d02
mascanho <mascanho@sapo.pt>
2025-03-08 17:46:45 +0000

    Update ResponseHeaders.tsx

--- app/global/_components/TablesContainer/SubTables/Headers/ResponseHeaders.tsx
+++ app/global/_components/TablesContainer/SubTables/Headers/ResponseHeaders.tsx
@@ -38,14 +38,14 @@ const ResponseHeaders: React.FC<ResponseHeadersProps> = ({ data, height }) => {
         {/* {error && <div className="text-red-600 mb-4 text-xs">{error}</div>} */}
 
         <div className="bg-white dark:bg-brand-darker dark:text-white rounded pb-3">
-          <h3 className="m-0 text-xs font-semibold text-xs pl-4 dark:text-white/50 mb-1">
+          <h3 className="m-0 text-xs font-semibold text-xs pl-1 dark:text-white/50 mb-1">
             Response Headers
           </h3>
           <div>
             {headers.map((header, index) => (
               <div
                 key={index}
-                className="grid grid-cols-[300px_1fr] gap-2 px-4  border-b border-gray-200 last:border-b-0 dark:border-white/30 text-xs flex items-center"
+                className="grid grid-cols-[300px_1fr] gap-2 px-1  border-b border-gray-200 last:border-b-0 dark:border-white/30 text-xs flex items-center"
               >
                 <span className="font-mono font-semibold text-xs text-black dark:text-white break-all ">
                   {header[0]}
