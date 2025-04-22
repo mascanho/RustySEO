a774c5a800bea6e1d3453642aa5d5d610eac3dde
Marco Guerreiro <mascanho@sapo.pt>
2025-04-22 19:25:29 +0100

    Update WidgetLogs.tsx

--- app/serverlogs/_components/WidgetLogs.tsx
+++ app/serverlogs/_components/WidgetLogs.tsx
@@ -249,10 +249,10 @@ export default function WidgetLogs() {
                           style={{ color: COLORS[idx % COLORS.length] }}
                         >
                           {entry.value.toLocaleString()}{" "}
-                          <span className="p-1 -mr-1 bg-slate-500 rounded-md text-white ml-2">
+                          <span className="px-1 py-[2px] -mr-1 bg-slate-500 rounded-md text-white ml-2">
                             {Math.round(
                               (Number(entry.value) / entries.length) * 100,
-                            )}{" "}
+                            )}
                             %
                           </span>{" "}
                         </span>
