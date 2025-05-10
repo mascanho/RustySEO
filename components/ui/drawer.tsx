0d1328b2bcb0fb3e87847057e3436a16c027ef6a
Marco Guerreiro <mascanho@sapo.pt>
2025-05-10 11:29:33 +0100

    Update drawer.tsx

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -28,7 +28,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn("fixed inset-0 z-0 bg-black/80", className)}
+    className={cn("fixed inset-0 z-30 bg-black/80", className)}
     {...props}
   />
 ));
@@ -43,7 +43,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-0 mt-24 flex h-auto flex-col rounded-t-[10px] border border-slate-200 bg-white dark:border-slate-800 dark:bg-slate-950",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border border-slate-200 bg-white dark:border-slate-800 dark:bg-slate-950",
         className,
       )}
       {...props}
