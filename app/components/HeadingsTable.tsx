5132582d43c33b4929be052c7ed21279c728e608
mascanho <mascanho@sapo.pt>
2025-07-08 12:06:09 +0100

    Update HeadingsTable.tsx

--- app/components/HeadingsTable.tsx
+++ app/components/HeadingsTable.tsx
@@ -24,10 +24,8 @@ const HeadingsTable = ({
   const setRepeatedHeadings = useOnPageSeo((state) => state.setHeadings);
 
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState(false); // State for custom dropdown
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [viewAIHeadings, setViewAIHeadings] = useState(false);
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState(false); // State for custom dropdown
   const previousHeadingsRef = useRef<string[]>(headings);
 
   const aiHeadings = headings.toString();
