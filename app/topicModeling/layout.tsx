829c311ed0fac4750ed7500a0c2cdd4ee4b6909b
Marco Guerreiro <mascanho@sapo.pt>
2026-01-01 00:01:01 +0000

    updates

--- app/topicModeling/layout.tsx
+++
@@ -1,6 +0,0 @@
-export const dynamic = "force-static";
-import MenuDrawer from "../components/ui/MenuDrawer";
-
-export default function ImagesLayout({ children }: any) {
-  return <main className="-mt-20 h-full">{children}</main>;
-}
