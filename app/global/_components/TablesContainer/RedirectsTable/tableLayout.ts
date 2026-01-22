c6f98ed6c4bbed10a5ddfc0518c6be0fc260efec
Marco Guerreiro <mascanho@sapo.pt>
2026-01-22 09:49:35 +0000

    fix: redirects table ui

--- app/global/_components/TablesContainer/RedirectsTable/tableLayout.ts
+++ app/global/_components/TablesContainer/RedirectsTable/tableLayout.ts
@@ -1,13 +1,13 @@
 export const initialColumnWidths = [
   "50px", // ID
-  "minmax(400px, auto)", // Page
+  "minmax(600px, auto)", // Page
   "80px", // Redirected
-  "minmax(400px, auto)", // Destination
+  "minmax(600px, auto)", // Destination
   "80px", // Type
   "80px", // Status
   "60px", // Hops
   "60px", // Loop
-  "minmax(400px, auto)", // Chain
+  "minmax(600px, auto)", // Chain
 ];
 
 export const initialColumnAlignments = [
