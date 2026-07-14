27ae6427524c50a8fbdd9b239cd48f0e9d69ed65
Marco Guerreiro <mascanho@sapo.pt>
2026-07-14 13:18:56 +0100

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -24,16 +24,16 @@ function Calendar({
         months:
           "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 relative",
         month: "space-y-4",
-        month_caption: "flex justify-center pt-1 relative items-center",
+        month_caption: "flex justify-center pt-1 items-center",
         caption_label: "text-sm font-medium",
         nav: "flex items-center",
         button_previous: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1 top-1",
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1 top-1 z-10",
         ),
         button_next: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-1 top-1",
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-1 top-1 z-10",
         ),
         month_grid: "w-full",
         weekdays: "flex",
